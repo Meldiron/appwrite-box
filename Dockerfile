8c7f1ca18783143ca501403098769dfe3497b80c
Matej Bačo <matejbaco2000@gmail.com>
2024-08-24 12:22:54 +0000

    Add Utopia

--- Dockerfile
+++ Dockerfile
@@ -6,10 +6,17 @@ RUN apk update && apk add docker-cli docker-cli-compose nodejs npm
 
 RUN docker-php-ext-configure pcntl --enable-pcntl && docker-php-ext-install pcntl
 
+RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin --filename=composer
+
 RUN npm i -g appwrite-cli@6.0.0-rc.9
+
+COPY composer.json composer.json
+
+RUN composer install
 
 COPY appwrite appwrite
 
 COPY src src
+
 
 CMD ["php", "src/app.php"]
--- src/app.php
+++ src/app.php
@@ -1,28 +1,37 @@
 <?
 
+require_once '../vendor/autoload.php';
+
 declare(ticks = 1);
+
+use Utopia\CLI\Console;
+use Utopia\Fetch\Client;
+
 pcntl_signal(SIGINT, function() {
     die;
     exit();
 });
 
+$output = '';
+
 echo "Cleaning up previous environment ..." . "\n";
 
-\shell_exec('docker rm --force $(docker ps -aq --filter "label=com.docker.compose.project.config_files=/root/appwrite/docker-compose.yml")');
-\shell_exec('cd appwrite && docker compose down -v');
+Console::execute('docker rm --force $(docker ps -aq --filter "label=com.docker.compose.project.config_files=/root/appwrite/docker-compose.yml")', '', $output);
+
+Console::execute('cd appwrite && docker compose down -v', '', $output);
 
 echo "Pulling Appwrite images ..." . "\n";
 
-\shell_exec("cd appwrite && docker compose pull");
+Console::execute("cd appwrite && docker compose pull", '', $output);
 
 echo "Starting new environment ..." . "\n";
 
-\shell_exec("cd appwrite && docker compose up -d");
+Console::execute("cd appwrite && docker compose up -d", '', $output);
 
 echo "Waiting for environment to be ready ... " . "\n";
 
 while(true) {
-    $response = \shell_exec('curl -s -o /dev/null -w "%{http_code}" http://172.17.0.1:9000/v1/account');
+    $response = Console::execute('curl -s -o /dev/null -w "%{http_code}" http://172.17.0.1:9000/v1/account', '', $output);
 
     if(\is_string($response) && \trim($response) === "401") {
         break;
@@ -34,13 +43,27 @@ while(true) {
 
 echo "Preparing account ... " . "\n";
 
-\shell_exec('curl -s \'http://172.17.0.1:9000/v1/account\' --data-raw \'{"userId":"unique()","email":"admin@appwrite.box","password":"password","name":"Appwrite Box"}\'');
+$client = new Client();
+$client->addHeader('content-type', Client::CONTENT_TYPE_APPLICATION_JSON);
 
-\shell_exec('curl -s \'http://172.17.0.1:9000/v1/account/sessions/email\' --data-raw \'{"email":"admin@appwrite.box","password":"password"}\'');
+$resp = $client->fetch(
+    url: "http://172.17.0.1:9000/v1/account",
+    method: 'POST',
+    body: [
+        'userId' => 'unique()',
+        'email' => 'admin@appwrite.box',
+        'password' => 'password',
+        'name' => 'Appwrite Box'
+    ]
+);
+
+die();
+
+\shell_exec('curl -w "%{http_code}" -X POST -H "Content-Type: application/json" -s \'http://172.17.0.1:9000/v1/account/sessions/email\' --data-raw \'{"email":"admin@appwrite.box","password":"password"}\'');
 
 echo "Preparing organization ... " . "\n";
 
-\shell_exec('curl -s \'http://172.17.0.1:9000/v1/teams\' --data-raw \'{"teamId":"appwrite-box","name":"Appwrite Box"}\'');
+\shell_exec('curl -w "%{http_code}" -X POST -H "Content-Type: application/json" -s \'http://172.17.0.1:9000/v1/teams\' --data-raw \'{"teamId":"appwrite-box","name":"Appwrite Box"}\'');
 
 echo "Preparing project ... " . "\n";
 
@@ -50,7 +73,7 @@ $json = \json_decode($json, true);
 $projectId = $json['projectId'];
 $projectName = $json['projectName'] ?? 'Unnamed';
 
-\shell_exec('curl -s \'http://172.17.0.1:9000/v1/projects\' --data-raw \'{"projectId":"' . $projectId . '","name":"' . $projectName . '","teamId":"appwrite-box","region":"default"}\'');
+\shell_exec('curl -w "%{http_code}" -X POST -H "Content-Type: application/json" -s \'http://172.17.0.1:9000/v1/projects\' --data-raw \'{"projectId":"' . $projectId . '","name":"' . $projectName . '","teamId":"appwrite-box","region":"default"}\'');
 
 echo "Pushing configuration ... " . "\n";
 
